0b557e5473953da6747fd79e2ef4e6bb6cdf3403
Amjad Khalfan <i4mjad@gmail.com>
2023-01-21 00:46:28 +0400

    chore, test the new use case on console playground project

---
+++ RihalChallenge/RihalChalleng.ConsolePlayground/GetStudentsConsoloePresenter.cs
@@ -0,0 +1,20 @@
+﻿using RihalChallenge.Domain.UseCases.GetStudentsUseCase;
+
+namespace RihalChallenge.ConsolePlayground;
+public class GetStudentsConsolePresenter: IGetStudentsPresenter
+{
+    public void Success(GetStudentsResponse response)
+    {
+        var students = response.Students;
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Name: {student.StudentName}, Class: {student.ClassName}, Country: {student.CountryName}, Date of birth: {student.DayOfBirth}");
+        }
+    }
+
+    public void Error(string error)
+    {
+        throw new NotImplementedException();
+    }
+}
+
--- RihalChallenge/RihalChalleng.ConsolePlayground/Program.cs
+++ RihalChallenge/RihalChalleng.ConsolePlayground/Program.cs
@@ -1,5 +1,10 @@
 ﻿//TODO: This is a playground for testing the application buisness logic since I still have a small knowledge about Blazor. 
 using Microsoft.Extensions.DependencyInjection;
+using RihalChallenge.ConsolePlayground;
+using RihalChallenge.Domain.DataSources;
+using RihalChallenge.Domain.Repositories;
+using RihalChallenge.Domain.Repositories.InMemory;
+using RihalChallenge.Domain.UseCases.GetStudentsUseCase;
 
 
 public class Program
@@ -9,15 +14,38 @@ public class Program
         //DI Container
         var serviceProvider = new ServiceCollection()          
             .AddSingleton<IFooService, BooService>()
+            .AddScoped<IInMemoryDataSource, InMemoryDataSource>()
+            .AddScoped<IStudentsRepository, InMemoryStudentsRepository>()
+            .AddScoped<IGetStudentsPresenter, GetStudentsConsolePresenter>()
+            .AddScoped<IGetStudentsUseCase, GetStudentsUseCase>()
             .BuildServiceProvider();
 
         //Program starts here
-        var foo = serviceProvider.GetService<IFooService>();
-        foo.DoSomething();
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("Chose From the list: ");
+        Console.WriteLine("1. Get All Students");
+
+        var input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                ListAllUsers();
+                break;
+        }
 
         
 
     }
+
+    public static void ListAllUsers()
+    {
+        var getAllUsersUseCase = new GetStudentsUseCase(new InMemoryStudentsRepository(new InMemoryDataSource()));
+        getAllUsersUseCase.Execute(new GetStudentsConsolePresenter());
+    }
+
+
+
 }
 
 //TODO: For testing purposes, to be deleted
