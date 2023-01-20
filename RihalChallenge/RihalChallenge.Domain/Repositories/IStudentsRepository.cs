5be845595b1e0a3550391c10c29fc5f28bf56738
Amjad Khalfan <i4mjad@gmail.com>
2023-01-21 00:44:34 +0400

    feat, create in memory repository to return students fake data

---
+++ RihalChallenge/RihalChallenge.Domain/Repositories/IStudentsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RihalChallenge.Domain.Entities;
+
+namespace RihalChallenge.Domain.Repositories;
+public interface IStudentsRepository
+{
+    public Task<IEnumerable<Student>> GetAllStudents();
+}
---
+++ RihalChallenge/RihalChallenge.Domain/Repositories/InMemory/InMemoryStudentsRepository.cs
@@ -0,0 +1,48 @@
+﻿using RihalChallenge.Domain.DataModels;
+using RihalChallenge.Domain.DataSources;
+using RihalChallenge.Domain.Entities;
+using RihalChallenge.Domain.UseCases.GetStudentsUseCase;
+
+namespace RihalChallenge.Domain.Repositories.InMemory;
+
+public class InMemoryStudentsRepository: IStudentsRepository
+{
+    private readonly IInMemoryDataSource _inMemoryDataSource;
+
+    public InMemoryStudentsRepository(IInMemoryDataSource inMemoryDataSource)
+    {
+        _inMemoryDataSource = inMemoryDataSource;
+    }
+    public Task<IEnumerable<Student>> GetAllStudents()
+    {
+        var allStudentsDataModel = _inMemoryDataSource.StudentsDataSet().GetAll();
+
+        return Task.FromResult(allStudentsDataModel.Select(GetStudentFromDataModel));
+    }
+
+    private Student GetStudentFromDataModel(StudentDataModel studentDataModel)
+    {
+        return new Student()
+        {
+            Id = studentDataModel.Id,
+            DayOfBirth = studentDataModel.DayOfBirth,
+            StudentName = studentDataModel.Name,
+            ClassName = GetClassName(studentDataModel.ClassId),
+            CountryName = GetCountryName(studentDataModel.CountryId)
+        };
+    }
+
+    private string GetClassName(Guid classId)
+    {
+        var classesDataModels = _inMemoryDataSource.ClassDataSet().GetAll();
+        var classDataModel = classesDataModels.First(x => x.Id == classId);
+        return classDataModel.Name;
+    }
+
+    private string GetCountryName(Guid classId)
+    {
+        var countriesDataModels = _inMemoryDataSource.CountryDataSet().GetAll();
+        var countryDataModel = countriesDataModels.First(x => x.Id == classId);
+        return countryDataModel.Name;
+    }
+}
