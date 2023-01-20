f725019692bf0adecdf63257d252b1e9881a1782
Amjad Khalfan <i4mjad@gmail.com>
2023-01-21 00:43:52 +0400

    feat, create in memory data set with fake data

---
+++ RihalChallenge/RihalChallenge.Domain/DataSources/DataSet.cs
@@ -0,0 +1,29 @@
+﻿namespace RihalChallenge.Domain.DataSources;
+
+public class DataSet<T>
+{
+    public List<T> Items;
+
+    public void Add(T item)
+    {
+        Items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return Items;
+    }
+
+    public void Delete(T item)
+    {
+        Items.Remove(item);
+    }
+
+    public void Update(T current,T updated)
+    {
+        Items[Items.IndexOf(current)] = updated;
+    }
+
+
+
+}
---
+++ RihalChallenge/RihalChallenge.Domain/DataSources/IInMemoryDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RihalChallenge.Domain.DataModels;
+
+namespace RihalChallenge.Domain.DataSources;
+public interface IInMemoryDataSource
+{
+    public DataSet<StudentDataModel> StudentsDataSet();
+    public DataSet<ClassDataModel> ClassDataSet();
+    public DataSet<CountryDataModel> CountryDataSet();
+}
